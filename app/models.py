50bed8847e720d7e54a539d61f3707f53699b0ea
sunday Nwuguru <sunday.nwuguru@andela.com>
2016-05-10 23:48:07 +0100

    Added more columns and relations to models

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
 from app import db
+from datetime import datetime
 
 class BucketListModel(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(255), unique=True)
+    items = db.relationship('BucketListItemModel', backref='bucketlist', lazy='dynamic')
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
     def __init__(self, name):
     	self.name = name
@@ -14,6 +19,10 @@ class BucketListModel(db.Model):
 class BucketListItemModel(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     task = db.Column(db.String(255))
+    done = db.Column(db.Boolean(), default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    bucketlist_id = db.Column(db.Integer, db.ForeignKey('bucketlist.id'))
 
     def __init__(self, task):
     	self.task = task
@@ -26,6 +35,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     email = db.Column(db.String(200), unique=True)
+    bucketlists = db.relationship('BucketList', backref='user')
 
     def __init__(self, username, email):
         self.username = username
