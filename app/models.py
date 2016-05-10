6c525e5a521a6fc4ebf9dabd6433994f8f7fc7e5
sunday Nwuguru <sunday.nwuguru@andela.com>
2016-05-10 23:27:50 +0100

    Added User and BucketListItem Model

--- app/models.py
+++ app/models.py
@@ -4,5 +4,32 @@ class BucketListModel(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(255), unique=True)
 
-    def __init__(self):
-    	print 'here'
+    def __init__(self, name):
+    	self.name = name
+
+   	def __repr__():
+   		return '<BucketListModel %r>' % self.name
+
+
+class BucketListItemModel(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    task = db.Column(db.String(255))
+
+    def __init__(self, task):
+    	self.task = task
+
+   	def __repr__():
+   		return '<BucketListItemModel %r>' % self.name
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(200), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
--- app/views.py
+++ app/views.py
@@ -3,12 +3,12 @@ from flask import  jsonify, abort, make_response, request, url_for
 from flask_restful import  Api
 from app.resource import *
 
-api = Api(app)
+api = Api(app, '/api/v1.0')
 
-api.add_resource(BucketLists, '/api/v1.0/bucketlists')
-api.add_resource(BucketList, '/api/v1.0/bucketlists/<id>')
-api.add_resource(BucketListItems, '/api/v1.0/bucketlists/<id>/items')
-api.add_resource(BucketListItem, '/api/v1.0/bucketlists/<id>/items/<item_id>')
+api.add_resource(BucketLists, '/bucketlists')
+api.add_resource(BucketList, '/bucketlists/<id>')
+api.add_resource(BucketListItems, '/bucketlists/<id>/items')
+api.add_resource(BucketListItem, '/bucketlists/<id>/items/<item_id>')
 
 @app.route('/')
 @app.route('/index')
