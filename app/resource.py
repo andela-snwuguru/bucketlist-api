392b0d7950860ac9647a22cea202f6bf78b196c5
sunday Nwuguru <sunday.nwuguru@andela.com>
2016-05-15 21:08:49 +0100

    [Feature] Complete implementation of bucketlists endpoint

--- app/resource.py
+++ app/resource.py
@@ -1,34 +1,132 @@
 from flask_restful import reqparse, abort, Resource
+from flask_httpauth import HTTPBasicAuth
+from flask import request, make_response, jsonify
 from app.models import *
 from app.helper import *
 
-
+auth = HTTPBasicAuth()
 parser = reqparse.RequestParser()
+
 parser.add_argument('task')
 
+@auth.verify_password
+def verify_token(username, password):
+    """
+    This method will verify the token and allow access to any resource that requires authentication
+    """
+    global token
+    token = request.headers.get('AccessToken','')
+    if not token:
+        return False
+
+    return decrypt(token)
+
+
+@auth.error_handler
+def unauthorized():
+    """
+    This will return a JSON error 403 response when token validation fails
+    """
+    return make_response(jsonify({'error': 'Unauthorized access','code':403}), 403)
 
 class BucketList(Resource):
+    @auth.login_required
     def get(self, id):
-        return {'id':id}
+        user_id = get_user_id_from_token(token)
+        bucketlist = BucketListModel.query.filter_by(id=id, created_by=int(user_id)).first()
+        if not bucketlist:
+            abort(403, message="Unauthorized access")
 
+        return {'data':bucketlist.get()}
+
+    @auth.login_required
     def delete(self, id):
-        return '', 204
+        user_id = get_user_id_from_token(token)
+        bucketlist = BucketListModel.query.filter_by(id=id, created_by=int(user_id)).first()
+        if not bucketlist:
+            abort(403, message="Unauthorized access")
 
+        if not delete(bucketlist):
+            abort(401, message="Unable to delete record")
+
+        return {}, 204
+
+    @auth.login_required
     def put(self, id):
-        #args = parser.parse_args()
-        #task = {'task': args['task']}
-        return {}, 201
+        if not validate_required_fields(request, ['name']):
+            abort(400, message="Missing required parameter")
+
+        user_id = get_user_id_from_token(token)
+        bucketlist = BucketListModel.query.filter_by(id=id, created_by=int(user_id)).first()
+        if not bucketlist:
+            abort(403, message="Unauthorized access")
+
+        bucketlist.name = request.json['name']
+
+        if not save(bucketlist):
+            abort(409, message="Item already exists")
+
+        return {'data':bucketlist.get()}, 201
 
 
 class BucketLists(Resource):
-    def get(self):
-        bucketlists = BucketListModel.query.all()
-        return [bucketlist.get() for bucketlist in bucketlists]
 
+    @auth.login_required
+    def get(self):
+        """
+        This endpoint returns list of bucketlists based on access token.
+        Method: GET
+        Parameters:
+            limit (optional)    default=25
+            page  (optional)    default=1
+        Header:   
+            AccessToken  (required)
+
+        Response: JSON
+        """
+        limit = int(request.args.get('limit',25))
+        page = int(request.args.get('page',1))
+        offset = (page * limit) - limit
+        user_id = get_user_id_from_token(token)
+        next_page = page + 1
+        prev_page = page - 1 if page > 1 else 1
+        all_list = BucketListModel.query.filter_by(created_by=int(user_id)).all()
+        bucketlists = all_list[offset:(limit + offset)]
+        result = {'data':[bucketlist.get() for bucketlist in bucketlists],'page':{}}
+        if (limit + offset) < len(all_list):
+            result['page']['next'] = "/api/v1.0/bucketlists?limit=" + str(limit) + "&page=" + str(next_page)
+        
+        if offset:
+            result['page']['prev'] = "/api/v1.0/bucketlists?limit=" + str(limit) + "&page=" + str(prev_page)
+
+        return result 
+
+    @auth.login_required
     def post(self):
-        #args = parser.parse_args()
-        #todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        return {}, 201
+        """
+        This endpoint returns created bucketlist details.
+        Method: POST
+        Parameters:
+            name (required) 
+        Header:   
+            AccessToken  (required)
+
+        Response: JSON
+        """
+
+        if not validate_required_fields(request, ['name']):
+            abort(400, message="Missing required parameter")
+
+        user_id = get_user_id_from_token(token)
+        user = User.query.filter_by(id=user_id).first()
+        if not user:
+            abort(403, message="Unauthorized access")
+
+        bucketlist = BucketListModel(request.json['name'], user)
+        if not save(bucketlist):
+            abort(409, message="Item already exists")
+
+        return {'data': bucketlist.get()}, 201
 
 
 class BucketListItem(Resource):
@@ -52,4 +150,56 @@ class BucketListItems(Resource):
         #args = parser.parse_args()
         #todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
         return {}, 201
+
+
+
+class Login(Resource):
+    def post(self):
+        """
+        This will authenticate user and provide token used to access other resources
+
+        Method: POST
+        Parameters: 
+            username    (required)
+            password    (required)
+        Response: JSON
+        """
+
+        if not validate_required_fields(request,['username','password']):
+            abort(400)
+
+        user = User.query.filter_by(username=request.json['username'],password=md5(request.json['password'])).first()
+
+        if not user:
+            abort(403)
+
+        return {'token': user.generate_token(),'data': user.get()}, 200
+
+
+class Register(Resource):
+
+    def post(self):
+        """
+        This will register a new user and returns the user credentials
+        
+        Method: POST
+        Parameters: 
+            username    (required)
+            email       (required)
+            password    (optional) default=''
+
+        Response: JSON
+        """
+
+        if not validate_required_fields(request, ['username','email']):
+            abort(400)
+
+        user = User(request.json['username'], request.json['email'],md5(request.json.get('password', "")))
+        if not save(user):
+            abort(401)
+
+        return {'data': user.get()}, 201
+
+
+
 
--- app/views.py
+++ app/views.py
@@ -3,8 +3,10 @@ from flask import  jsonify, abort, make_response, request, url_for
 from flask_restful import  Api
 from app.resource import *
 
-api = Api(app, '/api/v1.0')
+api = Api(app, prefix='/api/v1.0')
 
+api.add_resource(Login, '/auth/login')
+api.add_resource(Register, '/auth/register')
 api.add_resource(BucketLists, '/bucketlists')
 api.add_resource(BucketList, '/bucketlists/<id>')
 api.add_resource(BucketListItems, '/bucketlists/<id>/items')
@@ -14,66 +16,3 @@ api.add_resource(BucketListItem, '/bucketlists/<id>/items/<item_id>')
 @app.route('/index')
 def index():
     return "Hello, BucketList!"
-
-@app.route('/api/v1.0/auth/register', methods=['POST'])
-def register():
-	"""
-	This will register a new user and returns the user credentials
-	
-	Endpoint: /auth/register
-	Method: POST
-	Parameters: username, email, password
-	Response: JSON
-	"""
-    if not validate_data(request, ['username','email']):
-        abort(400)
-
-    user = User(request.json['username'], request.json['email'],md5(request.json.get('password', "")))
-    if not save(user):
-    	abort(401)
-
-    return jsonify({'data': user.get()}), 201
-
-@app.route('/api/v1.0/auth/login', methods=['POST'])
-def login():
-	"""
-	This will authenticate user and provide token used to access other resources
-
-	Endpoint: /auth/login
-	Method: POST
-	Parameters: username, password
-	Response: JSON
-	"""
-    if not validate_data(request,['username','password']):
-        abort(400)
-
-    user = User.query.filter_by(username=request.json['username'],password=md5(request.json['password'])).first()
-    if not user:
-    	abort(403)
-
-    return jsonify({'token': user.generate_token(),'data': user.get()}), 200
-
-# handling errors 405
-@app.errorhandler(405)
-def not_found(error):
-	return make_response(jsonify({'error': 'Invalid request method ' + request.method,'code':405}), 405)
-
-# handling errors 404
-@app.errorhandler(404)
-def not_found(error):
-	return make_response(jsonify({'error': 'Resource not found ','code':404}), 404)
-
-# handling errors 403
-@app.errorhandler(403)
-def not_found(error):
-	return make_response(jsonify({'error': 'Unauthorized access','code':403}), 403)
-
-# handling errors 400
-@app.errorhandler(400)
-def not_found(error):
-	return make_response(jsonify({'error': 'Bad request','code':400}), 400)
-
-# handling errors 401
-@app.errorhandler(401)
-def not_found(error):
-	return make_response(jsonify({'error': 'Unable to save records','code':401}), 401)
